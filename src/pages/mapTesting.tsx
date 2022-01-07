9bf50df26041af25d4629308db8a699105917f85
KimKwon <khj9709@hanyang.ac.kr>
2022-01-08 02:27:28 +0900

    chore: add map Testing page #3

---
+++ src/pages/mapTesting.tsx
@@ -0,0 +1,47 @@
+import useMap from 'hooks/useMap';
+import Head from 'next/head';
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
+
+function Home() {
+  const mapRef = useRef(null);
+  const [input, setInput] = useState('');
+  const { displayMarkerByAddress, moveByAddress } = useMap(mapRef);
+  return (
+    <>
+      <Head>
+        <title>소담, 소품샵 여정의 이야기를 담다</title>
+        <meta name="description" content="소담, 소품샵 여정의 이야기를 담다" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <input
+          placeholder="ex) 서울 서초구 방배중앙로29길 7"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={async () => await displayMarkerByAddress(input)}>마커 찍기</button>
+        <button onClick={() => moveByAddress(input)}>위치 이동</button>
+        <MapContainer ref={mapRef} />
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  background-color: #eeeeff;
+`;
+
+const MapContainer = styled.div`
+  width: 500px;
+  height: 500px;
+`;
+
+export default Home;
