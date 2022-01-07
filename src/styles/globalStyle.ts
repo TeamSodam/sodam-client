637b852aa74bb91ca11545a727416df59bfdf21b
Sohee KWON <soheekwon@naver.com>
2022-01-07 13:23:00 +0900

    fix: eslint EOL 설정 추가

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -2,18 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle` 
-    ${reset}
-    * {
-      box-sizing: border-box;
-    }
-    html, body {
-      width: 100%;
-      height: 100%;  
-    }
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  html, body {
+    width: 100%;
+    height: 100%;  
+  }
 
-    button:hover {
-      cursor: pointer
-    }
+  button:hover {
+    cursor: pointer
+  }
 `;
 
 export default GlobalStyles;
