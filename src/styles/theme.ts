57becf5934ebb6f4934d6267aa71aa18972a81cb
KimKwon <khj9709@hanyang.ac.kr>
2022-01-11 01:15:40 +0900

    chore: :gear: 테마/tsconfig 체리픽 #13

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,15 @@ export const theme = {
   colors: {
     tooltipTitle: '#57524C',
     tooltipSub: '#95908C',
+    purpleText: '#9796FE',
+    purpleMain: '#ABACFE',
+    blue: '#D9E1FC',
+    purpleBg: '#EEEEFF',
+    yellow: '#FFF1D8',
+    black1: '#312E36',
+    black2: '#514C57',
+    gray1: '#8C8C95',
+    gray2: '#E9E8EB',
+    grayBg: '#F8F8F8',
   },
 };
