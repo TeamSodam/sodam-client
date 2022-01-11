705bce31293ce2284cefc412b828af04f1327a8f
KimKwon <khj9709@hanyang.ac.kr>
2022-01-11 15:27:54 +0900

    feat: :sparkles: 소품샵 지도 레이아웃 구현 #17

--- src/pages/map/[areaId].tsx
+++ src/pages/map/[areaId].tsx
@@ -1,16 +1,28 @@
-import SEOUL_ENUM from 'constants/SeoulAreaEnum';
+import useMap from 'hooks/useMap';
 import { useRouter } from 'next/router';
+import LeftArr from 'public/assets/ic_leftArr.svg';
+import { useRef } from 'react';
 import styled from 'styled-components';
 
 function MapWithAreaId() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  useMap(mapRef);
   const router = useRouter();
   const { areaId } = router.query;
+
+  const onClickGoBack = () => {
+    router.push('/map');
+  };
 
   if (!areaId) return <div>something wrong!</div>;
 
   return (
     <StyledContainer>
-      <TESTING_HEADER>{`${SEOUL_ENUM[+areaId]} 을 클릭하셨어요!`}</TESTING_HEADER>
+      <StyledGoBack onClick={onClickGoBack}>
+        <LeftArrIC />
+        <span>지역 다시 선택하기</span>
+      </StyledGoBack>
+      <MapContainer ref={mapRef} />
     </StyledContainer>
   );
 }
@@ -21,9 +33,36 @@ const StyledContainer = styled.main`
   margin: 7.2rem 18.75% 0 18.75%;
 `;
 
-const TESTING_HEADER = styled.h1`
-  font-size: 3rem;
-  color: ${({ theme }) => theme.colors.purpleMain};
+const StyledGoBack = styled.button`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  gap: 2.2rem;
+
+  background-color: transparent;
+  border: none;
+  & > span {
+    font-weight: 700;
+    font-size: 1.4rem;
+    line-height: 2rem;
+    color: ${({ theme }) => theme.colors.black2};
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const LeftArrIC = styled(LeftArr)`
+  height: 2rem;
+  fill: ${({ theme }) => theme.colors.black2};
+`;
+
+const MapContainer = styled.div`
+  width: 100%;
+  height: 82.4rem;
+
+  margin: 3.5rem 0 13.2rem 0;
 `;
 
 export default MapWithAreaId;
