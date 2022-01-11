2b01afb3346f02d9be95a81c1ff778d043ca0ce3
KimKwon <khj9709@hanyang.ac.kr>
2022-01-11 23:51:43 +0900

    feat: :sparkles: ShopElement 컴포넌트 구현 및 더미데이터 생성 #17

--- src/components/MapSidebar/index.tsx
+++ src/components/MapSidebar/index.tsx
@@ -4,38 +4,121 @@ import StarIC from 'public/assets/ic_star.svg';
 import { useState } from 'react';
 import styled, { css } from 'styled-components';
 
-interface StyledIsOpenProps {
-  isOpen: boolean;
+import ShopElement, { ShopElementProps } from './ShopElement';
+
+interface StyledMapProps {
+  isOpen?: boolean;
 }
+
+const dummyShopList: ShopElementProps[] = [
+  {
+    shopId: 1,
+    shopName: '소품샵 최대길이 소품샵 최대길이 소품 분류와 8px 남기고',
+    category: ['인테리어소품'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 동교로46길 33',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 35,
+  },
+  {
+    shopId: 2,
+    shopName: '수바코',
+    category: ['문구.팬시'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 희우정로20길 66 상가1층2호',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 42,
+  },
+  {
+    shopId: 3,
+    shopName: '프레젠트모먼트',
+    category: ['인테리어소품'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 동교로 49-1 1층',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 54,
+  },
+  {
+    shopId: 4,
+    shopName: '무드',
+    category: ['인테리어소품', '캔들'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 포은로8길 16',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 10,
+  },
+  {
+    shopId: 5,
+    shopName: '제로스페이스',
+    category: ['문구.팬시'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 희우정로16길 32',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 189,
+  },
+  {
+    shopId: 6,
+    shopName: '브라와',
+    category: ['문구.팬시'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 희우정로10길 30 1층',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 200,
+  },
+  {
+    shopId: 7,
+    shopName: '망원만물',
+    category: ['인테리어소품'],
+    image: '/assets/dummy/dummy-shop.png',
+    address: '서울 마포구 동교로 35 왼쪽가게',
+    close: '영업시간 12:00~17:00',
+    reviewCount: 200,
+  },
+];
 
 function MapSidebar() {
   const [isOpen, setIsOpen] = useState(true);
   const toggle = () => setIsOpen((prevState) => !prevState);
+
+  const optionList = [
+    {
+      icon: ActivePinIC,
+      label: '인기 순',
+    },
+    {
+      icon: StarIC,
+      label: '내가 저장한',
+    },
+  ];
+
+  const showOptions = () =>
+    optionList.map((option) => (
+      <Option key={option.label}>
+        <option.icon />
+        <span>{option.label}</span>
+      </Option>
+    ));
+
   return (
     <StyledSidebar isOpen={isOpen}>
       {isOpen && (
         <>
-          <SortOptions>
-            <Option>
-              <ActivePinIC />
-              <span>인기 순</span>
-            </Option>
-            <Option>
-              <StarIC />
-              <span>내가 저장한</span>
-            </Option>
-          </SortOptions>
-          <ShopList />
+          <OptionList>{showOptions()}</OptionList>
+          <ShopList>
+            {dummyShopList.map((dummy) => (
+              <ShopElement shopInfo={dummy} key={dummy.shopId} />
+            ))}
+          </ShopList>
         </>
       )}
-
       <ToggleBtn onClick={toggle}>
         <ToggleArrowIcon isOpen={isOpen} />
       </ToggleBtn>
     </StyledSidebar>
   );
 }
-const StyledSidebar = styled.aside<StyledIsOpenProps>`
+
+const StyledSidebar = styled.aside<StyledMapProps>`
   position: absolute;
   top: 0;
   left: 0;
@@ -50,7 +133,7 @@ const StyledSidebar = styled.aside<StyledIsOpenProps>`
   transition: width ease-in 100ms;
 `;
 
-const SortOptions = styled.ul`
+const OptionList = styled.ul`
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -91,6 +174,10 @@ const ShopList = styled.ul`
   flex: 5.22;
   display: flex;
   flex-direction: column;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const ToggleBtn = styled.button`
@@ -107,7 +194,7 @@ const ToggleBtn = styled.button`
   border: 0.1rem solid ${({ theme }) => theme.colors.gray2};
 `;
 
-const ToggleArrowIcon = styled(LeftArrIC)<StyledIsOpenProps>`
+const ToggleArrowIcon = styled(LeftArrIC)<StyledMapProps>`
   ${(props) =>
     !props.isOpen &&
     css`
