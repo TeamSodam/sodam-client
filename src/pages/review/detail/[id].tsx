f591d8927522cae174cedf6897b807e1ddbfeb3c
Hwang Juhee <julia0276@naver.com>
2022-01-15 21:18:59 +0900

    feat: :sparkles: 리뷰 상세페이지 상단 영역 퍼블리싱 #26

--- src/pages/review/detail/[id].tsx
+++ src/pages/review/detail/[id].tsx
@@ -1,5 +1,251 @@
+import ReviewCard from 'components/common/ReviewCard';
+import Image from 'next/image';
+import styled from 'styled-components';
+
 function Detail() {
-  return <div>리뷰 상세</div>;
+  return (
+    <StyledContainer>
+      <Header>
+        <ShopName>마인띵스</ShopName>
+        <ShopInfo>문구·팬시, 인테리어소품</ShopInfo>
+      </Header>
+      <ReviewDetailCardContainer>
+        <ReviewDetailCardHeader>
+          <div className="profile">
+            <Image src={'/assets/profile.jpeg'} alt="profile" width={48} height={48} />
+            <ReviewInfo>
+              <ReviewWriter>슈슉슈슈슉슈슈발로마</ReviewWriter>
+              <ReviewWriteDate>2022년 01월 05일</ReviewWriteDate>
+            </ReviewInfo>
+          </div>
+          <IconContainer>
+            <LikeReview>
+              <Image
+                className="review_icon"
+                src={'/assets/likeReviewIcon.svg'}
+                width={29}
+                height={26}
+                alt="like"
+              />
+              <p>35</p>
+            </LikeReview>
+            <SaveReview>
+              <Image
+                className="review_icon"
+                src={'/assets/scrapReviewIcon.svg'}
+                width={22}
+                height={26}
+                alt="save"
+              />
+              <p>102</p>
+            </SaveReview>
+          </IconContainer>
+        </ReviewDetailCardHeader>
+        <ReviewThumbnail>
+          <Image
+            className="review_thumbnail"
+            src={'/assets/thumbnail.jpeg'}
+            width={788}
+            height={405}
+            alt="thumbnail"
+          />
+        </ReviewThumbnail>
+        <ReviewImageSlider>
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+          <Image src={'/assets/profile.jpeg'} alt="image" width={75} height={75} />
+        </ReviewImageSlider>
+        <ReviewTextInfo>
+          <ReviewProductInfo>
+            <p>인테리어소품</p>
+            <p> : </p>
+            <p>10,000 - 14,990원</p>
+          </ReviewProductInfo>
+          <ReviewContent>
+            리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용
+            더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자
+            리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용
+            더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자
+            리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용
+            더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자
+            리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용
+            더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자
+            리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용
+            더미텍스트 500자 리뷰용 더미텍스트 500자 리뷰용 더
+          </ReviewContent>
+        </ReviewTextInfo>
+        <ReviewTagList>
+          <ReviewTag>#여덟글자해시태그</ReviewTag>
+          <ReviewTag>#여덟글자해시태그</ReviewTag>
+          <ReviewTag>#여덟글자해시태그</ReviewTag>
+          <ReviewTag>#여덟글자해시태그</ReviewTag>
+          <ReviewTag>#여덟글자해시태그</ReviewTag>
+        </ReviewTagList>
+      </ReviewDetailCardContainer>
+      <OtherReviewList>
+        <ReviewListHeader>
+          <HeaderTitle>이 소품샵의 다른 리뷰</HeaderTitle>
+          <Filter />
+        </ReviewListHeader>
+        {/* <ReviewCard /> */}
+      </OtherReviewList>
+    </StyledContainer>
+  );
 }
+const StyledContainer = styled.div`
+  width: 79.2rem;
+  margin: 7.2rem 56.4rem 8rem 56.4rem;
+`;
+
+const Header = styled.header`
+  margin-bottom: 4rem;
+`;
+
+const ShopName = styled.h2`
+  font-size: 3rem;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.black2};
+  margin-bottom: 0.7rem;
+`;
+
+const ShopInfo = styled.h3`
+  font-size: 1.8rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+
+const ReviewDetailCardContainer = styled.div`
+  width: 100%;
+  height: 107.7rem;
+  border: solid 2px ${({ theme }) => theme.colors.gray2};
+  border-radius: 10px;
+  margin-bottom: 5.6rem;
+`;
+const ReviewDetailCardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 4.1rem 4.1rem 2.3rem 4.1rem;
+  .profile {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    img {
+      border-radius: 50px;
+    }
+  }
+`;
+const ReviewInfo = styled.div`
+  margin-left: 1.6rem;
+`;
+
+const ReviewWriter = styled.p`
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 1.6rem;
+  margin-bottom: 0.7rem;
+  color: ${({ theme }) => theme.colors.black1};
+`;
+
+const ReviewWriteDate = styled.p`
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 1.6rem;
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+const IconContainer = styled.div`
+  display: flex;
+  font-size: 1.4rem;
+  color: ${({ theme }) => theme.colors.purpleMain};
+`;
+const LikeReview = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 3.5rem;
+
+  & > p {
+    margin-top: 0.4rem;
+  }
+`;
+const SaveReview = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > p {
+    margin-top: 0.4rem;
+  }
+`;
+
+const ReviewThumbnail = styled.div`
+  margin-bottom: 3.2rem;
+`;
+
+const ReviewImageSlider = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50rem;
+  margin: 0 auto 4.1rem auto;
+
+  img {
+    border-radius: 10px;
+  }
+`;
+
+const ReviewTextInfo = styled.div`
+  width: 70.8rem;
+  margin: 0 auto 2.6rem;
+`;
+
+const ReviewProductInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2.1rem;
+  font-size: 1.4rem;
+  font-weight: 400;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.purpleText};
+`;
+
+const ReviewContent = styled.p`
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 3.2rem;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+
+const ReviewTagList = styled.div`
+  width: 70.8rem;
+  margin: 0 auto 2.6rem;
+`;
+
+const ReviewTag = styled.span`
+  display: inline-block;
+  height: 2.4rem;
+  line-height: 1.3rem;
+  margin-right: 0.7rem;
+  padding: 0.2rem 1rem;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: solid 2px ${({ theme }) => theme.colors.purpleText};
+  border-radius: 30px;
+  color: ${({ theme }) => theme.colors.purpleText};
+  font-size: 1.2rem;
+`;
+const OtherReviewList = styled.div``;
+
+const ReviewListHeader = styled.div``;
+
+const HeaderTitle = styled.h3`
+  font-size: 2.4rem;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.black1};
+`;
+
+const Filter = styled.div``;
 
 export default Detail;
