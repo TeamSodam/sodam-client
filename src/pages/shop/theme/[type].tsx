1af40629754d5ce2ec8dbd34064e80daa60124a7
KimKwon <khj9709@hanyang.ac.kr>
2022-01-17 22:35:40 +0900

    feat: :sparkles: 테마소품샵 데이터 불러오기 및  그리드 구현 #44

--- src/pages/shop/theme/[type].tsx
+++ src/pages/shop/theme/[type].tsx
@@ -1,7 +1,21 @@
+import { wrapper } from 'app/store';
+import DropDownFilter from 'components/common/DropDownFilter';
+import ShopCard from 'components/common/ShopCard';
 import ThemeSelector from 'components/ThemeSelector';
+import { shopApi } from 'features/shops/shopApi';
 import styled from 'styled-components';
+import { Shop, ShopThemeType } from 'types/shop';
 
-function Theme() {
+type ThemePageProps = {
+  [key in ShopThemeType]: Shop[];
+};
+
+function ThemePage(props: ThemePageProps) {
+  const { 아기자기한 } = props;
+
+  const showCurrentThemeList = () =>
+    아기자기한.map((shop) => <ShopCard key={shop.shopId} cardData={shop} />);
+
   return (
     <Container>
       <Wrapper>
@@ -10,10 +24,43 @@ function Theme() {
         </ThemeWrapper>
       </Wrapper>
       <Delimiter />
-      <Wrapper />
+      <DropDownWrapper>
+        <DropDownFilter pageType="theme" />
+      </DropDownWrapper>
+
+      <ShopList>{showCurrentThemeList()}</ShopList>
     </Container>
   );
 }
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async () => {
+  const dispatch = store.dispatch;
+
+  const THEME_LIST: ShopThemeType[] = ['아기자기한', '힙한', '모던한', '빈티지'];
+  let resultData = {};
+
+  for (const theme of THEME_LIST) {
+    const result = await dispatch(
+      shopApi.endpoints.getShopByTheme.initiate({
+        theme,
+        sortType: 'popular',
+        offset: 9,
+        limit: 1,
+      }),
+    );
+
+    console.log(result);
+
+    resultData = {
+      ...resultData,
+      [theme]: result.data,
+    };
+  }
+
+  return {
+    props: resultData,
+  };
+});
 
 const Container = styled.main`
   width: 100%;
@@ -43,4 +90,19 @@ const Delimiter = styled.div`
   margin: 0;
 `;
 
-export default Theme;
+const ShopList = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 4rem 2.4rem;
+
+  margin-bottom: 8.1rem;
+`;
+
+const DropDownWrapper = styled.div`
+  padding-right: 18.75%;
+  margin-left: auto;
+  margin: 4rem 0 2.4rem auto;
+`;
+
+export default ThemePage;
