0641238c49f9d7d269ed0dbe834e862b4db9ee05
Huiseon Jeon <huiseon37@gmail.com>
2022-01-18 06:04:50 +0900

    feat: ✨ 검색 기능 구현 #31

--- src/components/review/ShopSearch.tsx
+++ src/components/review/ShopSearch.tsx
@@ -1,12 +1,42 @@
+import { shopApi } from 'features/shops/shopApi';
 import Image from 'next/image';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
+import { Shop } from 'types/shop';
+
+import ShopSearchList from './ShopSearchList';
 
 function ShopSearch() {
+  const [inputValue, setInputValue] = useState('');
+  const [searchValue, setSearchValue] = useState<Shop[] | undefined>();
+  const [trigger] = shopApi.useLazyGetShopSearchResultQuery();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    if (!(e.target instanceof HTMLFormElement)) return;
+    e.preventDefault();
+    const result = await trigger(inputValue);
+    setSearchValue(result.data);
+  };
+
   return (
     <StyledRoot>
-      <span>소품샵명을 입력해주세요 (필수)</span>
-      <Image src={'/assets/searchIcon.svg'} width={27} height={27} alt="search" />
+      <StyledWrapper>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="소품샵명을 입력해주세요 (필수)"
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </form>
+        <Image src={'/assets/searchIcon.svg'} width={27} height={27} alt="search" />
+      </StyledWrapper>
+      {searchValue && <ShopSearchList shopList={searchValue} />}
     </StyledRoot>
   );
 }
@@ -15,6 +45,11 @@ export default ShopSearch;
 
 const StyledRoot = styled.div`
   display: flex;
+  position: relative;
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
   align-items: center;
   justify-content: space-between;
   width: 48.6rem;
@@ -22,12 +57,21 @@ const StyledRoot = styled.div`
   border-radius: 0.5rem;
   background-color: ${theme.colors.grayBg};
   padding: 0 1.6rem;
-  cursor: pointer;
 
-  & > span {
-    color: ${theme.colors.gray1};
-    font-weight: 500;
-    font-size: 1.4rem;
-    line-height: 2rem;
+  & > form {
+    width: 100%;
+    & > input {
+      font-weight: 500;
+      font-size: 1.4rem;
+      line-height: 2rem;
+      width: 100%;
+      background-color: ${theme.colors.grayBg};
+      border: 0;
+      outline: 0;
+
+      & > ::placeholder {
+        color: ${theme.colors.gray1};
+      }
+    }
   }
 `;
---
+++ src/components/review/ShopSearchList.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
+import { Shop } from 'types/shop';
+
+interface StyledSSLProps {
+  shopList: Shop[] | undefined;
+}
+function ShopSearchList(props: StyledSSLProps) {
+  const { shopList } = props;
+
+  return (
+    <StyledUl>
+      {shopList?.map((item) => (
+        <li key={item.shopId}>{item.store}</li>
+      ))}
+    </StyledUl>
+  );
+}
+export default ShopSearchList;
+
+const StyledUl = styled.ul`
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  top: 5.6rem;
+  left: 0;
+  flex-direction: column;
+  width: 48.6rem;
+  box-shadow: 0px 3px 8px rgba(87, 82, 76, 0.15);
+  border-radius: 0.5rem;
+  background-color: ${theme.colors.grayBg};
+  padding: 0.8rem;
+
+  & > li {
+    height: 3.6rem;
+    line-height: 3.6rem;
+    font-weight: 500;
+    font-size: 1.4rem;
+    color: ${theme.colors.black2};
+    cursor: pointer;
+  }
+  &>li: hover {
+    background-color: ${theme.colors.purpleBg};
+  }
+`;
