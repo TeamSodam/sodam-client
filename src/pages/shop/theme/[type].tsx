90e0e23cd00b8f591417db1dfee01507ed5c0170
KimKwon <khj9709@hanyang.ac.kr>
2022-01-17 21:01:27 +0900

    css: :art: 테마별 소품샵 레이아웃 #44

--- src/pages/shop/theme/[type].tsx
+++ src/pages/shop/theme/[type].tsx
@@ -4,17 +4,43 @@ import styled from 'styled-components';
 function Theme() {
   return (
     <Container>
-      <ThemeSelector />
+      <Wrapper>
+        <ThemeWrapper>
+          <ThemeSelector />
+        </ThemeWrapper>
+      </Wrapper>
+      <Delimiter />
+      <Wrapper />
     </Container>
   );
 }
 
 const Container = styled.main`
   width: 100%;
-
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 7.2rem 18.75% 0 18.75%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const ThemeWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  height: calc(50.8rem - 7.2rem);
+`;
+
+const Delimiter = styled.div`
+  width: 100%;
+  height: 0.5px;
+  background-color: ${({ theme }) => theme.colors.navLine};
+  padding: 0;
+  margin: 0;
 `;
 
 export default Theme;
