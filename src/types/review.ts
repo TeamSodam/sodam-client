12842954baf223fff33fe457e679209037aab548
KimKwon <khj9709@hanyang.ac.kr>
2022-01-19 23:46:16 +0900

    chore: :gear: 타입 재정의 #63

--- src/types/review.ts
+++ src/types/review.ts
@@ -7,21 +7,62 @@ export interface Item {
 export interface Review {
   shopId: number;
   shopName: string;
-  category: string[] | string;
+  category: string[];
   reviewId: number;
-  date: Date | string;
-  isLiked: boolean;
-  isScraped: boolean;
+  date: Date;
   likeCount: number;
   scrapCount: number;
+  isLiked: boolean;
+  isScraped: boolean;
+  writerThumbnail: string;
+  writerName: string;
   image: string[];
   item: Item[];
   content: string;
   tag: string[];
-  writerThumbnail: string;
-  writerName: string;
   thumbnail: string;
   reviewCount: number;
+}
+
+export interface ReviewRecentResponse {
+  reviewId: number;
+  writerName: string;
+  content: string;
+  likeCount: number;
+  scrapCount: number;
+  image: string;
+  shopName: string;
+  category: string[];
+  writerThumbnail: string;
+}
+
+export interface ReviewByShopIdResponse {
+  reviewId: number;
+  shopId: number;
+  image: string[];
+  userImage: string;
+  nickname: string;
+  likeCount: number;
+  scrapCount: number;
+  content: string;
+}
+
+export interface ReviewMyWriteResponse {
+  reviewId: number;
+  likeCount: number;
+  scrapCount: number;
+  content: string;
+  image: string;
+  date: Date;
+}
+
+export interface ReviewMyScrapResponse {
+  writerName: string;
+  content: string;
+  scrapCount: number;
+  likeCount: number;
+  shopName: string;
+  category: string;
 }
 
 export interface ReviewInfoRequestById {
