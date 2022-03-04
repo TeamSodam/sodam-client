ed11f2da4f7faa6dd9883a5409ce4c7ce1e4828c
Huiseon Jeon <huiseon37@gmail.com>
2022-03-04 19:32:33 +0900

    feat: ✨ userSignInfo type 정의 #115

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,3 +2,12 @@ export interface User {
   name: string;
   thumbnail: string;
 }
+
+export interface UserSingupInfo {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  themePreference: string;
+}
