f97d2ddc03ad5067a228804dd78c7ebb8eef35c1
Hwang Juhee <julia0276@naver.com>
2022-05-17 21:30:23 +0900

    chore: :gear: 태그 수정 및 코드리뷰 반영 #128

--- src/components/Password/PasswordChangeForm.tsx
+++ src/components/Password/PasswordChangeForm.tsx
@@ -8,18 +8,20 @@ function PasswordChangeForm() {
         <h3>비밀번호를 잊어버리셨나요?</h3>
       </StyledTitleWrapper>
       <StyledExplain>
-        <h3>소담에 가입했던 이메일을 입력해주세요</h3>
-        <h3>비밀번호 재설정에 필요한 인증번호를 전송해드립니다</h3>
+        <p>소담에 가입했던 이메일을 입력해주세요</p>
+        <p>비밀번호 재설정에 필요한 인증번호를 전송해드립니다</p>
       </StyledExplain>
-      <StyledInput placeholder="example@naver.com" />
-      <StyledWarningMessage>존재하지 않는 이메일입니다</StyledWarningMessage>
-      <StyledButton>인증번호 발송</StyledButton>
+      <StyledForm>
+        <input placeholder="example@naver.com" />
+        <p>존재하지 않는 이메일입니다</p>
+        <button type="submit">인증번호 발송</button>
+      </StyledForm>
     </StyledRoot>
   );
 }
 
-const StyledRoot = styled.div`
-  width: 100%;
+const StyledRoot = styled.section`
+  width: 39.1rem;
   height: 100%;
   padding: 12rem 0 16rem 0;
   display: flex;
@@ -45,7 +47,7 @@ const StyledExplain = styled.div`
   flex-direction: column;
   justify-content: space-between;
   text-align: center;
-  & > h3 {
+  & > p {
     font-weight: 500;
     font-size: 1.5rem;
     line-height: 2.2rem;
@@ -53,38 +55,44 @@ const StyledExplain = styled.div`
   }
 `;
 
-const StyledInput = styled.input`
-  width: 39.1rem;
-  height: 5rem;
-  margin-bottom: 2.8rem;
-  background-color: white;
-  border: 1px solid ${theme.colors.purpleText};
-  border-radius: 5px;
-  font-size: 1.3rem;
-  font-weight: 500;
-  padding-left: 1.6rem;
-  color: ${theme.colors.black2};
+const StyledForm = styled.form`
+  width: 100%;
 
-  & > :placeholder {
-    color: ${theme.colors.gray1};
+  & > input {
+    width: 100%;
+    height: 5rem;
+    margin-bottom: 2.8rem;
+    background-color: white;
+    border: 1px solid ${theme.colors.purpleText};
+    border-radius: 5px;
+    font-size: 1.3rem;
+    font-weight: 500;
+    padding-left: 1.6rem;
+    color: ${theme.colors.black2};
+
+    & ::placeholder {
+      color: ${theme.colors.gray1};
+    }
+  }
+
+  & > p {
+    text-align: center;
+    font-size: 1.4rem;
+    margin-bottom: 1.6rem;
+    line-height: 2rem;
+    color: ${theme.colors.purpleText};
+  }
+
+  & > button {
+    width: 100%;
+    height: 4.6rem;
+    background-color: ${theme.colors.gray2};
+    border-radius: 5px;
+    border: 0;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: 700;
   }
 `;
 
-const StyledWarningMessage = styled.h3`
-  font-size: 1.4rem;
-  margin-bottom: 1.6rem;
-  line-height: 2rem;
-  color: ${theme.colors.purpleText};
-`;
-
-const StyledButton = styled.button`
-  width: 39.1rem;
-  height: 4.6rem;
-  background-color: ${theme.colors.gray2};
-  border-radius: 5px;
-  border: 0;
-  color: white;
-  font-size: 1.5rem;
-  font-weight: 700;
-`;
 export default PasswordChangeForm;
