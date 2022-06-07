0037a0034632e04bf9f55a7ba72b57f1c9762327
sunny <huiseon37@gmail.com>
2022-06-07 19:24:06 +0900

    edit: :fire: import 경로 변경

--- src/features/users/userApi.ts
+++ src/features/users/userApi.ts
@@ -1,7 +1,7 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { axiosBaseQuery } from 'libs/api';
 import { SodamResponse } from 'types/api';
-import { UserImage, UserInfo, UserTheme } from 'types/user';
+import { UserImage, UserInfo, UserTheme } from 'types/auth';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
