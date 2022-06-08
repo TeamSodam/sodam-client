dd44c336810c7da8e25257a15a6bbc35c6d74dd4
sunny <huiseon37@gmail.com>
2022-06-08 15:15:10 +0900

    chore: :gear: auth.ts 파일 분리

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,35 +1,3 @@
-import { ShopThemeType } from './shop';
-
-export interface User {
-  name: string;
-  thumbnail: string;
-}
-type InputIndexType =
-  | 'name'
-  | 'nickname'
-  | 'email'
-  | 'emailConfirm'
-  | 'password'
-  | 'passwordConfirm';
-
-export interface UserInfo {
-  name: string;
-  nickname: string;
-  email: string;
-}
-export type UserTheme = ShopThemeType[];
-export interface UserImage {
-  image: string;
-}
-export type UserInfoAll = UserInfo & UserImage & UserTheme;
-export type UserSignupRequest = Record<
-  InputIndexType,
-  {
-    value: string | null;
-    isComplete: boolean;
-  }
-> & { themePreference: { value: string[]; isComplete: boolean } };
-
 export interface NicknameResponseType {
   uniqueNickname: boolean;
 }
@@ -46,3 +14,19 @@ export interface EmailResponseType {
   uniqueEmail: boolean;
   verificationCode: string;
 }
+
+type InputIndexType =
+  | 'name'
+  | 'nickname'
+  | 'email'
+  | 'emailConfirm'
+  | 'password'
+  | 'passwordConfirm';
+
+export type UserSignupRequest = Record<
+  InputIndexType,
+  {
+    value: string | null;
+    isComplete: boolean;
+  }
+> & { themePreference: { value: string[]; isComplete: boolean } };
