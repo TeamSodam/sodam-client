c65f17a40d3bfd3a41ea393b82d1aafc5e99c869
sunny <huiseon37@gmail.com>
2022-06-08 15:14:56 +0900

    chore: :gear: user.ts 파일 분리

---
+++ src/types/user.ts
@@ -0,0 +1,20 @@
+import { ShopThemeType } from './shop';
+
+export interface User {
+  name: string;
+  thumbnail: string;
+}
+
+export interface UserInfo {
+  name: string;
+  nickname: string;
+  email: string;
+}
+export type UserTheme = ShopThemeType[];
+export interface UserImage {
+  image: string;
+}
+export type UserInfoAll = UserInfo & UserImage & UserTheme;
+export interface UserTokenResponse {
+  accesstoken: string;
+}
