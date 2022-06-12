a9f9e0957dc66c650ce008ef70ca095dca7f4488
sohee <soheek@fairsquarelab.com>
2022-06-12 14:19:36 +0900

    feat: :sparkles: 테마별 소품샵 데스크탑 반응형

--- src/pages/shop/theme/[type].tsx
+++ src/pages/shop/theme/[type].tsx
@@ -66,6 +66,9 @@ const Container = styled.main`
 
 const Wrapper = styled.div`
   padding-top: 7.2rem;
+  ${applyMediaQuery('desktop')} {
+    padding-top: 5.2rem;
+  }
   ${applyMediaQuery('mobile')} {
     padding: 1.4rem 0;
   }
@@ -78,6 +81,10 @@ const ThemeWrapper = styled.div`
   width: 100%;
   display: flex;
   height: calc(50.8rem - 7.2rem);
+  ${applyMediaQuery('desktop')} {
+    height: max-content;
+    margin-bottom: 0.5rem;
+  }
   ${applyMediaQuery('mobile')} {
     height: unset;
   }
@@ -97,6 +104,10 @@ const ShopList = styled.div`
   grid-template-columns: repeat(4, 1fr);
   gap: 4rem 2.4rem;
   margin-bottom: 8.1rem;
+  ${applyMediaQuery('desktop')} {
+    gap: 4rem 1.6rem;
+    margin-bottom: 8rem;
+  }
   ${applyMediaQuery('tablet')} {
     grid-template-columns: repeat(3, 1fr);
     gap: 3.2rem 1.6rem;
@@ -110,6 +121,10 @@ const ShopList = styled.div`
 const DropDownWrapper = styled.div`
   margin-left: auto;
   margin: 4rem 0 2.4rem auto;
+  ${applyMediaQuery('desktop')} {
+    margin-top: 6.8rem;
+    margin-bottom: 2.4rem;
+  }
 `;
 
 export default ThemePage;
