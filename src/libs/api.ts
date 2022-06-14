36d944ede369c91fdfb64ae84f2204eb45a12d39
Jinny <khj9709@icloud.com>
2022-06-14 23:53:39 +0900

    chore: :clipboard: 불필요 주석 삭제 #168

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -3,7 +3,6 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { RootState } from 'app/store';
 import axios, { AxiosRequestConfig } from 'axios';
 import { logout, setToken } from 'features/users/userSlice';
-import { decodeJwt } from 'src/utils/decode-jwt';
 
 const BASE_URL = process.env.NODE_ENV === 'development' ? '/api' : 'https://server.sodam.me';
 
@@ -32,16 +31,7 @@ const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).reducer.user.token;
-    if (token) {
-      headers.set('accesstoken', token);
-      const decoded = decodeJwt(token);
-      if (decoded) {
-        const isExpired = decoded.exp * 1000 < new Date().getTime();
-        console.log(
-          isExpired ? '만료됐음' : `${(decoded.exp * 1000 - new Date().getTime()) / 1000}초 남음`,
-        );
-      }
-    }
+    if (token) headers.set('accesstoken', token);
 
     return headers;
   },
@@ -71,7 +61,6 @@ export const fetchBaseQueryWithToken: BaseQueryFn<
     if (refreshResult.statusText === 'OK') {
       const { accesstoken } = refreshResult.data.data;
       api.dispatch(setToken(accesstoken));
-      console.log(`${accesstoken}으로 갱신완료`);
       result = await baseQuery(args, api, extraOptions);
     } else api.dispatch(logout());
   }
