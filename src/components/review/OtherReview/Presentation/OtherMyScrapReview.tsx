1ec17e0f9d02868e0e2ad47a116cd1ad0f312ec6
Jinny <khj9709@icloud.com>
2022-08-13 20:23:57 +0900

    feat: :sparkles: 내가스크랩한다른리뷰 무한스크롤 적용 #194

--- src/components/review/OtherReview/Presentation/OtherMyScrapReview.tsx
+++ src/components/review/OtherReview/Presentation/OtherMyScrapReview.tsx
@@ -1,18 +1,56 @@
+import Loader from 'components/common/Loader';
 import ReviewCard from 'components/common/ReviewCard';
 import { useGetMyScrapReviewQuery } from 'features/reviews/reviewApi';
+import useInfiniteQuery from 'hooks/useInfiniteQuery';
+import { LoadPoint } from 'pages/shop/theme/[type]';
+import { useEffect, useState } from 'react';
+import { ReviewMyScrapResponse } from 'types/review';
+
+const PAGE_LIMIT = 4;
 
 function OtherMyScrapReview({ reviewId }: { reviewId: number }) {
   const { data: myScrapResponse } = useGetMyScrapReviewQuery();
+
+  const [paginatedData, setPaginatedData] = useState<ReviewMyScrapResponse[][]>([]);
+  const [offset, setOffset] = useState(0);
+
+  const fetchNextData = () => {
+    if (paginatedData.length === offset + 1) return [];
+    setOffset((prevOffset) => prevOffset + 1);
+    return paginatedData[offset + 1];
+  };
+
+  const {
+    data: infiteMyScrapData,
+    isLoading,
+    loadPointRef,
+  } = useInfiniteQuery(paginatedData[0], fetchNextData, {});
+
+  useEffect(() => {
+    if (myScrapResponse) {
+      const initialPaginatedData = myScrapResponse.reduce(
+        (acc, cur, idx) => {
+          const currentPage = Math.floor(idx / PAGE_LIMIT);
+          const currentPageData = acc[currentPage] ? [...acc[currentPage], cur] : [cur];
+          return [...acc.slice(0, currentPage), currentPageData];
+        },
+        [[]] as ReviewMyScrapResponse[][],
+      );
+
+      setPaginatedData(initialPaginatedData);
+    }
+  }, [myScrapResponse]);
 
   if (!myScrapResponse) return <div />; // 로더 삽입하면 좋을 것 같음.
 
   return (
     <>
-      {myScrapResponse
+      {infiteMyScrapData
         .filter((_) => _.reviewId !== reviewId)
         .map((myScrapReview) => (
           <ReviewCard key={myScrapReview.shopId} reviewData={myScrapReview} />
         ))}
+      <LoadPoint ref={loadPointRef}>{isLoading && <Loader />}</LoadPoint>
     </>
   );
 }
